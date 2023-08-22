af010812ff2332f868c08e6f3caf5902e7c064b4
WenHsi <wenxi0423@gmail.com>
2023-08-22 16:40:08 +0800

    create "ColorSelector"

--- HabitTracker/ContentView.swift
+++ HabitTracker/ContentView.swift
@@ -7,60 +7,83 @@
 
 import SwiftUI
 
+struct ColorSelector : View{
+    @Binding var habitColor: Color
+    var body: some View {
+        ColorPicker(selection: $habitColor){
+            Text("Color")
+                .font(.headline)
+                .fontWeight(.bold)
+        }
+        }
+}
+
+//struct IconSelector : View{
+//    @Binding var habitIcon : String
+//    let iconsList: [String]
+//    var body: some View{
+//
+//    }
+//}
 
 struct ContentView: View {
     @State private var isShowingSheet = false
     @State private var input = ""
+    @State private var habitColor = Color.blue
+    @State private var tempColor = Color.blue
+    
     var body: some View {
         NavigationView {
-            VStack{
-                Text(Date(), style: .date)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                HStack{
-                    VStack(alignment: .leading){
-                        HStack(alignment: .firstTextBaseline){
-                            Image(systemName:"book.fill").foregroundColor(Color.white)
-                                .padding(.vertical)
-                            Text("abcdefghij").font(.title3).fontWeight(.bold).foregroundColor(Color.white)
-                        }
-                        Spacer()
-                        
-
-                        GeometryReader { geometry in
-                            let circleSize = min(geometry.size.width, geometry.size.height) * 0.6
-                            HStack{
+            ScrollView{
+                VStack{
+                    Text(Date(), style: .date)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    HStack{
+                        VStack(alignment: .leading){
+                            HStack(alignment: .firstTextBaseline){
+                                Image(systemName:"book.fill").foregroundColor(Color.white)
+                                    .padding(.vertical)
+                                Text("abcdefghij").font(.title3).fontWeight(.bold).foregroundColor(Color.white)
+                            }
+                            Spacer()
+                            
+                            
+                            GeometryReader { geometry in
+                                let circleSize = min(geometry.size.width, geometry.size.height) * 0.6
+                                HStack{
                                     ForEach(0..<7) { index in
                                         Circle()
                                             .stroke(Color.white, lineWidth: 2)
                                             .frame(width: circleSize, height: circleSize)
                                             .padding(.leading, circleSize)
-                                        }
                                     }
                                 }
+                            }
+                            Spacer()
+                            
+                        }
                         Spacer()
-                        
+                        Button(action:{
+                            //button function
+                        }){
+                            
+                            Color.blue
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(10)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius:10)
+                                        .stroke(Color.white, lineWidth:2)
+                                )
+                            
+                        }
                     }
-                    Spacer()
-                    Button(action:{
-                        //button function
-                    }){
-                        
-                        Color.blue
-                            .frame(width: 80, height: 80)
-                            .cornerRadius(10)
-                            .overlay(
-                                RoundedRectangle(cornerRadius:10)
-                                    .stroke(Color.white, lineWidth:2)
-                            )
-                        
-                    }
-                }
                     .frame(width: 300, height: 80)
                     .padding()
                     .background(Color.blue)
                     .cornerRadius(10)
-                Spacer()
+                    Spacer()
+                }
             }
             .padding()
             .navigationBarTitleDisplayMode(.inline)
@@ -87,9 +110,8 @@ struct ContentView: View {
                                             .keyboardType(.default)
                                             .textFieldStyle(.roundedBorder).padding()
                                     }
+                                    ColorSelector(habitColor: $habitColor)
                                     
-                                    
-
                                 }
                                 
                         }
